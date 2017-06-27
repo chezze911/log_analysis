dbad408949b438fcb13b268f1e642d3db1acc8b2
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-06-27 14:03:05 -0700

    Initial commit

---
+++ log_analysis.py
@@ -0,0 +1,18 @@
+
+import psycopg2
+
+# Log Analysis Project #3
+# Udacity Full Stack Nanodegree
+
+# Store global database name
+DB_NAME = 'news'
+
+
+# Problem 1: What are the most popular three articles of all time?
+
+
+# Problem 2: Who are the most popular article authors of all time?
+
+
+# Problem 3: On which days did more than 1% of requests lead to errors?
+
