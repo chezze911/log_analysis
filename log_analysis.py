11ab37dd7c35f8891fbb495b7c831d2466b346bc
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-08-09 14:41:07 -0700

    Update problem 3 query

--- log_analysis.py
+++ log_analysis.py
@@ -5,51 +5,74 @@ import psycopg2
 # Udacity Full Stack Nanodegree
 
 # Store global database name
-DB_NAME = 'news'
+DBNAME = 'news'
 
 
 def execute_query(query):
     try:
-        db = psycopg2.connect('dbname=' + DB_NAME)
+        db = psycopg2.connect('dbname=' + DBNAME)
         c = db.cursor()
         c.execute(query)
         results = c.fetchall()
         db.close()
         return results
     except BaseException:
-        print "Unable to connect to the database.  Please try again."
-        
-        
-def print_query_results(query_results):
-    print query_results['title']
-    for result in query_results['results']:
-        print ('\t' + str(result[0]) + ' ---- ' + str(result[1]) + ' views')
+        print("Unable to connect to the database.  Please try again.")
 
+    
 # Problem 1: What are the most popular three articles of all time?
-query_1 = "SELECT title,views FROM article_view LIMIT 3"
+def top_three_articles():
+    query_1 = """SELECT title,views 
+                 FROM article_view 
+                 LIMIT 3;"""
     
-query_1_results = dict()
+    top_three_query = execute_query(query_1)
 
-query_1_results['title'] = "\n 1. Top Three Articles By Views \n"
-    
+    print("\n 1. Top Three Articles: \n")
+    for result in top_three_query:
+        print('\t' + str(result[0]) + ' ---- ' + str(result[1]) + ' views')
+
+
     
 
 # Problem 2: Who are the most popular article authors of all time?
-query_2 = ""
-query_2_results = dict()
-query_2_results['title'] = "Most Popular Article Authors By Views"
+def most_popular_authors():
+    query_2 = """SELECT authors.name, sum(article_view.views) AS views 
+                 FROM article_view,authors
+                 WHERE authors.id = article_view.author
+                 GROUP BY authors.name 
+                 ORDER BY views DESC;"""
+                 
+    most_popular_authors_query = execute_query(query_2)
+
+    print("\n 2. Most Popular Article Authors: \n")
+    for result in most_popular_authors_query:
+        print('\t' + str(result[0]) + ' ---- ' + str(result[1]) + ' views')
+
+
+
 
 # Problem 3: On which days did more than 1% of requests lead to errors?
-query_3 = ""
-query_3_results = dict()
-query_3_results['title'] = "High Error days with more than 1 percent error"
+def over_one_percent_error_days():
+    query_3 = """SELECT * 
+                 FROM error_rate 
+                 WHERE error_rate.percentage > 1 
+                 ORDER BY error_rate.percentage DESC;"""
+    
+    over_one_percent_error_days_query = execute_query(query_3)
+    
+    print("\n 3. High Error days with more than 1 percent error: \n")
+    
+    for result in over_one_percent_error_days_query:
+        print('\t' + str(result[0]) + ' ---- ' + str(result[1]) + ' % errors'.formate(date, percentage))
 
 
-# Store query results and print output
-query_1_results['results'] = execute_query(query_1)
 
-#print output
-print_query_results(query_1_results)
-
+if __name__ == '__main__':
+    
+    top_three_articles()
+    most_popular_authors()
+    over_one_percent_error_days()
+    
 
 
